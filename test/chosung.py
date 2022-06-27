12858452911b8ecc7a0d49d720d3f8670a164d3c
legendaryheropenguin <108207324+legendaryheropenguin@users.noreply.github.com>
2022-06-27 21:21:54 +0900

    Update chosung.py

--- test/chosung.py
+++ test/chosung.py
@@ -8,25 +8,6 @@ import os
 DIR_WORD = '../word/'
 
 # 함수 세팅 ------------------------
-
-def print_title():      # 맨 처음 타이틀 화면
-    print('=' * 30)
-    print('아름다운 우리말! 훈민정음 게임!\n\n\n')
-    print('로그인할 이름을 적어주세요\nx를 입력하면 게임을 종료합니다.')
-    print('=' * 30)
-
-def print_main():      # 메인 화면
-    print('=' * 30)
-    print('아름다운 우리말! 훈민정음 게임!\n\n\n')
-    print('1. 게임 시작')
-    print('2. 순위 확인')
-    print('3. 게임 설명')
-    print('x를 입력하면 초기 화면으로 돌아갑니다.')
-    print('=' * 30)
-
-def print_help():       # 게임 설명
-    print('애옹')
-
 def highscore():        # 랭킹 확인
     highscore = pd.read_excel('../rank.xlsx', engine='openpyxl', index_col=0, usecols=[0, 1, 2, 3])
     # rank.xlsx 열기, 시간까지만 보여주기
@@ -54,33 +35,8 @@ def save_result(player): # 게임 종료 후 결과 저장
     data.to_excel('../rank.xlsx')       # rank.xlsx에 덮어쓰기
 
 # point는 몇 개 맞췄는지 저장
-point = 20              # 테스트용 점수
+# point = 20              # 테스트용 점수
 # player = '리오니'        # 테스트용 닉네임
 #
 # save_result(player)     # 테스트용 함수
 
-
-# 게임 구현 ------------------------
-print('게임이 시작되었습니다.')
-
-while True:
-    print_title()
-    player = input("닉네임 : ") # 닉네임 미리 저장
-
-    if player != 'x' and player != 'X':     # x말고 다른거 누르면 다음 화면으로 넘어갈거야
-        while True:
-            print_main()
-            select = input("메뉴 선택 : ")
-
-            if select == '1': # 게임 시작
-                pass
-
-            elif select == '2': highscore()  # 랭킹 확인
-
-            elif select == '3': print_help() # 게임 설명
-
-            elif select == 'x' or select == 'X': break # x 누르면 초기화면으로 돌아갈거야
-
-            else: print('잘못된 접근입니다.')
-
-    elif player == 'x' or player == 'X': break # 게임 종료
