ff24302886cfec88867fbee9ffd1ff9deeb87d5b
legendaryheropenguin <108207324+legendaryheropenguin@users.noreply.github.com>
2022-06-27 21:18:19 +0900

    Update chosung.py

--- test/chosung.py
+++ test/chosung.py
@@ -1,21 +1,11 @@
 # 전역 변수 및 상수 --------------------------
-DIR_WORD = '../word/'
-DIR_PLAYER = '../player/'
-
 import time
 import numpy as np                  # 넘파이 np로 쓸거에요 우리
 import pandas as pd                 # 판다스는 pd로 쓸거에요 우리
 import openpyxl
 import os
 
-if not os.path.exists(DIR_WORD):    # word 폴더 존재 여부 체크 없으면 만들기
-    os.makedirs(DIR_WORD)
-
-if not os.path.exists(DIR_PLAYER):    # player 폴더 존재 여부 체크 없으면 만들기
-    os.makedirs(DIR_PLAYER)
-
-playerList = os.listdir(DIR_PLAYER)     # player 폴더 안 파일들의 이름들을 리스트로 만들기
-
+DIR_WORD = '../word/'
 
 # 함수 세팅 ------------------------
 
@@ -38,20 +28,37 @@ def print_help():       # 게임 설명
     print('애옹')
 
 def highscore():        # 랭킹 확인
-    highscore = pd.read_excel('./rank.xlsx', engine='openpyxl', index_col=0, usecols=[0, 1, 2, 3])
+    highscore = pd.read_excel('../rank.xlsx', engine='openpyxl', index_col=0, usecols=[0, 1, 2, 3])
     # rank.xlsx 열기, 시간까지만 보여주기
     highscore = highscore.loc[1:10]     # 10등까지만 보여주기
     print(highscore)
-    print(type(highscore))
 
-def save_result(player):      # 게임 결과 저장
-    player = player.replace(" ", "")    # 이름의 공백 제거
-    rank_time = time.localtime()  # 게임 결과 데이터 저장 시에 저장할 시간값
+def save_result(player): # 게임 종료 후 결과 저장
+    data = pd.read_excel('../rank.xlsx', engine='openpyxl', index_col=0) # 랭킹 엑셀 읽기
+    player = player.replace(' ','') # 공백 제거
+    rank_time = time.localtime()    # 현재 시간 저장
+    data.loc[11] = [player, point, time.strftime("%y/%m/%d %H:%M:%S", rank_time), f'{time.time():.2f}']
+    # 새로운 게임 결과를 11등 위치에 저장
 
-    filename = f'{DIR_PLAYER + player}.txt' # 저장할 결과값의 파일 이름
-    with open(filename, mode = 'a', encoding = 'utf-8') as file:
-        file.write(f'{time.strftime("%y/%m/%d %H:%M:%S", rank_time)} {time.time()}\n')
-        # 점수 저장, 보기 좋게 시간 표시 양식을 바꿔서 저장, 하나는 시간 비교용으로 원본값 저장
+    cols = ['점수', '시간값']
+    tups = data[cols].sort_values(by= cols, ascending=(False, True)).apply(tuple, axis=1)
+    # 점수, 시간값 2가지 기준에 따라 정렬
+    f, i = pd.factorize(tups)
+    factorized = pd.Series(f+1, tups.index)
+
+    data['rank'] = factorized  #새로운 순위를 'rank'행에 저장
+    data = data.sort_values(by=['rank']) # 'rank'에 따라 정렬
+    data.index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]    # 다시 1~10위 부여하기
+    data = data.drop('rank', axis=1)    # 'rank'행 삭제
+    data = data.drop(11)                # 11위 삭제
+    data.to_excel('../rank.xlsx')       # rank.xlsx에 덮어쓰기
+
+# point는 몇 개 맞췄는지 저장
+point = 20              # 테스트용 점수
+# player = '리오니'        # 테스트용 닉네임
+#
+# save_result(player)     # 테스트용 함수
+
 
 # 게임 구현 ------------------------
 print('게임이 시작되었습니다.')
