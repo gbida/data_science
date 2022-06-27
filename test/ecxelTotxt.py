c8a82231623ff1abcc60a9012d0eb1b370a78828
Son-BR <108207241+Son-BR@users.noreply.github.com>
2022-06-27 20:30:10 +0900

    Update ecxelTotxt.py

--- test/ecxelTotxt.py
+++ test/ecxelTotxt.py
@@ -2,40 +2,41 @@
 import pandas
 import os
 import re
+import tqdm
 
 # 폴더
-ori='./dic/'                    # 처리할 엑셀파일 위치
-txt_dir='./word/'               # 최종 txt 파일 저장 폴더
+ori='../dic/'                    # 처리할 엑셀파일 위치
+txt_dir='../word/'               # 최종 txt 파일 저장 폴더
 if not os.path.exists(txt_dir): os.mkdir(txt_dir)
 
 # 함수 정의
 
 # 엑셀 처리후 txt로 변환
 def ETT(excel):
-    if not os.path.exists(txt_dir+'ETT/'): os.mkdir(txt_dir+'ETT/')
-    data=pandas.read_excel(ori+excel)
+    if not os.path.exists(DIR_WORD+'ETT/'): os.mkdir(DIR_WORD+'ETT/')
+    data = pandas.read_excel(ori+excel)
     row=data.iloc[:,:3]
     col=row[((row['구성 단위']=='단어') & (row['고유어 여부']=='고유어')) | ((row['구성 단위']=='단어') & (row['고유어 여부']=='한자어'))]
     col=col.rename(columns={'어휘':'가'})
     txt=col.iloc[:,:1]
-    txt.to_csv(txt_dir+'ETT/'+excel[:-5]+'.txt', index=0)
+    txt.to_csv(DIR_WORD+'ETT/'+excel[:-5]+'.txt',index=0)
 
 # 한글 제외한 모든 문자 제거
 def clean_txt(txt_file):
-    if not os.path.exists(txt_dir+'clean_txt'): os.mkdir(txt_dir+'clean_txt')
-    with open(txt_dir+'ETT/'+txt_file, mode='r',encoding='utf-8') as txt:
-        with open(txt_dir+'clean_txt/'+txt_file, mode='w', encoding='utf-8') as txt2:
+    if not os.path.exists(DIR_WORD+'clean_txt'): os.mkdir(DIR_WORD+'clean_txt')
+    with open(DIR_WORD+'ETT/'+txt_file,mode='r',encoding='utf-8') as txt:
+        with open(DIR_WORD+'clean_txt/'+txt_file,mode='w',encoding='utf-8') as txt2:
             while True:
                 word=txt.readline()
-                word=re.sub('[^가-힣]', '', word)
+                word=re.sub('[^가-힣]','', word)
                 txt2.write(word+'\n')
                 if not word: break
 
 # 2글자 아닌 단어 제거
 def wordTwo(txt_file):
-    if not os.path.exists(txt_dir+'wordTwo'): os.mkdir(txt_dir+'wordTwo')
-    with open(txt_dir+'clean_txt/'+txt_file, mode='r',encoding='utf-8') as txt:
-        with open(txt_dir+'wordTwo/'+txt_file, mode='w',encoding='utf-8') as txt2:
+    if not os.path.exists(DIR_WORD+'wordTwo'): os.mkdir(DIR_WORD+'wordTwo')
+    with open(DIR_WORD+'clean_txt/'+txt_file,mode='r',encoding='utf-8') as txt:
+        with open(DIR_WORD+'wordTwo/'+txt_file, mode='w',encoding='utf-8') as txt2:
             while True:
                 word=txt.readline()
                 if len(word)==3:
@@ -46,9 +47,9 @@ def wordTwo(txt_file):
 
 # 중복제거
 def set_txt(txt_file):
-    if not os.path.exists(txt_dir+'set_txt'): os.mkdir(txt_dir+'set_txt')
-    with open(txt_dir+'wordTwo/'+txt_file, mode='r', encoding='utf-8') as txt:
-        with open(txt_dir+'set_txt/'+txt_file, mode='w', encoding='utf-8') as txt2:
+    if not os.path.exists(DIR_WORD+'set_txt'): os.mkdir(DIR_WORD+'set_txt')
+    with open(DIR_WORD+'wordTwo/'+txt_file,mode='r',encoding='utf-8') as txt:
+        with open(DIR_WORD+'set_txt/'+txt_file,mode='w',encoding='utf-8') as txt2:
             txtList = list(set(txt.readlines()))
             txtList.sort()
             for i in txtList: txt2.write(i)
@@ -56,7 +57,7 @@ def set_txt(txt_file):
 # 초성 판별 후 ㄱㄱ~ㅎㅎ.txt에 각각 저장
 def chosung(txt_file):
     chosungList = ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ']
-    with open(txt_dir+'set_txt/'+txt_file, mode='r', encoding='utf-8') as txt:
+    with open(DIR_WORD+'set_txt/'+txt_file,mode='r',encoding='utf-8') as txt:
         while True:
             word=txt.readline().strip()
             chosung1=''
@@ -65,28 +66,32 @@ def chosung(txt_file):
                 for i in list(word):
                     code=((ord(i)-ord('가'))//588)
                     chosung1+=chosungList[code]
-                with open(txt_dir+chosung1+'.txt',mode='a',encoding='utf-8') as txt2:
+                with open(DIR_WORD+chosung1+'.txt',mode='a',encoding='utf-8') as txt2:
                     txt2.write(word+'\n')
 
+# 폴더 내 파일 개수 반환
+def count_file(dir_path):
+    dir_count= os.listdir(dir_path)
+    return len(dir_count)
+
 # 기능 구현
-excelList=os.listdir(ori)
-for i in excelList:
-    ETT(i)
-    i=i[:-5]+'.txt'
-    clean_txt(i)
-    wordTwo(i)
-    set_txt(i)
-    chosung(i)
-
-
-
-# 필요없는 파일,폴더 삭제
-reList=os.listdir(txt_dir)
-dirlist=[]
-for i in reList:
-    if not '.txt' in i:
-        dirlist+=[i]
-for i in dirlist:
-    for j in os.listdir(txt_dir+i):
-        os.remove(txt_dir+i+'/'+j)
-    os.rmdir(txt_dir+i)
+if count_file(DIR_WORD)!=351:
+    excelList=os.listdir(ori)
+    for i in tqdm.tqdm(excelList,desc='Loading... ',ncols=80):
+        ETT(i)
+        i=i[:-5]+'.txt'
+        clean_txt(i)
+        wordTwo(i)
+        set_txt(i)
+        chosung(i)
+        
+    # 필요없는 파일,폴더 삭제
+    reList=os.listdir(DIR_WORD)
+    dirlist=[]
+    for i in reList:
+        if not '.txt' in i:
+            dirlist+=[i]
+    for i in dirlist:
+        for j in os.listdir(DIR_WORD+i):
+            os.remove(DIR_WORD+i+'/'+j)
+        os.rmdir(DIR_WORD+i)
