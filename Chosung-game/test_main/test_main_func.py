6d1251865f5e01459b1bcdefbfbbfcb4006ce845
jh5209 <wlgp5209@gmail.com>
2022-06-28 13:55:57 +0900

    Update test_main_func.py

--- Chosung-game/test_main/test_main_func.py
+++ Chosung-game/test_main/test_main_func.py
@@ -4,44 +4,80 @@ import pandas as pd
 import random
 import openpyxl
 
+
 DIR_WORD_PATH = '../dic/word/'
 FILE_EXCEL_PATH = './rank.xlsx'
 
-# 초성 분리
-def getChosung(word: str) -> str:
-    result = ''
-    for w in word:
-        result += chr(((ord(w) - 44032) // 588) + 4352)
-    return result
 
-# 게임 시작
-def startGame(player: str) -> tuple:
-    global score
-    score = 0
-    answerlist = []
-    i = 0
-    # 초성 랜덤 제시
-    if player:
-        testfile = []
-        wordfile = random.choice(os.listdir(DIR_WORD_PATH))
-        with open(wordfile, encoding='utf-8') as f:
-            for line in f.readlines():
-                testfile.append(line.replace('\n', ''))
-        correct = random.choice(testfile)
-        correct_chs = getChosung(correct)
-        # player 대답 저장
-        # answerlist.append(chosung_input)
-        if answerlist[i] == correct:
-            if answerlist[i] not in answerlist:
-                score += 1
-                i += 1
-                return correct, score
+player1 = None
+player2 = None
+
+
+# set a data frame for players
+class player:
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+    # set instance values of player
+    def setname(self, name):
+        self.name = name
+        return self.name
+
+    def setscore(self, score):
+        self.score += score
+        return self.score
+
+    # get instance values of player
+    def getname(self):
+        return self.name
+
+    def getscore(self):
+        return self.score
+
+
+# takes players info
+def setplayer(name):
+    """Return player1 as a player class type object if new,
+     otherwise player2 as a same object for existing users"""
+    global player1, player2
+    if player1 is None:
+        player1 = player(name)
+        return player1
+    else:
+        player2 = player(name)
+        return player2
+
+
+# seperate chosung from a random word
+def getchosung(word):
+    """Return chosung as a string from a two letters word"""
+    if isinstance(word, str):
+        chosung = ''
+        for w in word:
+            chosung += chr(((ord(w) - 44032) // 588) + 4352)
+        return chosung
+
+
+# to show chosung to player
+def givechosung():
+    """Return two letters word and its chosung as a tuple """
+    pickword = []  # 'ㄷㅇ.txt'
+    randomfile = random.choice(os.listdir(DIR_WORD_PATH))
+    with open(randomfile, encoding='utf-8') as f:
+        for line in f.readlines():
+            pickword.append(line.replace('\n', ''))
+    correct = random.choice(pickword)  # '단어'
+    correct_chs = getchosung(correct)  # 'ㄷㅇ'
+    return correct, correct_chs
+
 
 # 랭킹 확인
 def highscore():
     # rank.xlsx 열기, 시간
     highscore=pd.read_excel(FILE_EXCEL_PATH, engine='openpyxl', index_col=0, usecols=[0, 1, 2, 3])
     return highscore.loc[1:10]  # 10등까지만 보여주기
+
 
 # 게임 종료 후 결과 저장
 def save_result(player):
@@ -65,3 +101,20 @@ def save_result(player):
     data=data.drop('rank', axis=1)  # 'rank'행 삭제
     data=data.drop(11)  # 11위 삭제
     data.to_excel(FILE_EXCEL_PATH)  # rank.xlsx에 덮어쓰기
+
+
+if __name__ == '__main__':
+    name = input('create a player name')
+    setplayer(name)
+
+    print(givechosung()[1])
+    player_say = input('say answer')
+
+    while True:
+        try:
+            if player_say == givechosung()[0]:
+                setplayer(name).setScore(1)
+            else: print(givechosung()[1])
+        except Exception as e:
+            print(e)
+            break
