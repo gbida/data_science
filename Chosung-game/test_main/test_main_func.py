9fb661ea80f29d7a03f08ef8d81c0fa20d9fa39f
legendaryheropenguin <108207324+legendaryheropenguin@users.noreply.github.com>
2022-06-28 10:59:45 +0900

    Update test_main_func.py

--- Chosung-game/test_main/test_main_func.py
+++ Chosung-game/test_main/test_main_func.py
@@ -1,8 +1,11 @@
 import time
 import pandas as pd
 import os
+import openpyxl
 import random
 
+DIRC_WORD = '../dic/word/'
+DIRC_EXCEL = './rank.xlsx'
 
 # 초성 분리
 def getchs(word: str) -> str:
@@ -20,7 +23,7 @@ def startchs(player: str) -> tuple:
     # 초성 랜덤 제시
     if player:
         testfile = []
-        wordfile = random.choice(os.listdir('../dic/word/'))
+        wordfile = random.choice(os.listdir(DIRC_WORD))
         with open(wordfile, encoding='utf-8') as f:
             for line in f.readlines():
                 testfile.append(line.replace('\n', ''))
@@ -36,20 +39,20 @@ def startchs(player: str) -> tuple:
 
 # 랭킹 확인
 def highscore():
-    # test_main_rank.xlsx 열기, 시간
-    highscore=pd.read_excel('./test_main_rank.xlsx', engine='openpyxl', index_col=0, usecols=[0, 1, 2, 3])
+    # rank.xlsx 열기, 시간
+    highscore=pd.read_excel(DIRC_EXCEL, engine='openpyxl', index_col=0, usecols=[0, 1, 2, 3])
     return highscore.loc[1:10]  # 10등까지만 보여주기
 
 # 게임 종료 후 결과 저장
 def save_result(player):
-    data=pd.read_excel('./test_main_rank.xlsx', engine='openpyxl', index_col=0)  # 랭킹 엑셀 읽기
+    data=pd.read_excel(DIRC_EXCEL, engine='openpyxl', index_col=0)  # 랭킹 엑셀 읽기
     player=player.replace(' ', '')  # 공백 제거
     rank_time=time.localtime()  # 현재 시간 저장
     data.loc[11]=[player, score, time.strftime("%y/%m/%d %H:%M:%S", rank_time), f'{time.time():.2f}']
 
     # 새로운 게임 결과를 11등 위치에 저장
     cols=['점수', '시간값']
-    tups=data[cols].sort_values(by=cols, ascending=(False, True)).apply(tuple, axis=1)
+    tups=data[cols].sort_values(by=cols, ascending=(False, False)).apply(tuple, axis=1)
 
     # 점수, 시간값 2가지 기준에 따라 정렬
     f, i=pd.factorize(tups)
@@ -61,4 +64,4 @@ def save_result(player):
     data.index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]  # 다시 1~10위 부여하기
     data=data.drop('rank', axis=1)  # 'rank'행 삭제
     data=data.drop(11)  # 11위 삭제
-    data.to_excel('./test_main_rank.xlsx')  # rank.xlsx에 덮어쓰기
+    data.to_excel(DIRC_EXCEL)  # rank.xlsx에 덮어쓰기
