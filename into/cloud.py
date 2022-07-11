2f5f4f4649effe69aeeac3b76aef740965a06691
gbida <vekosky@gmail.com>
2022-07-12 00:03:21 +0900

    try to open chrome.exe by python

---
+++ into/cloud.py
@@ -0,0 +1,10 @@
+import os
+
+# change direcotry and open chrome
+os.chdir("C:\\Program Files\\Google\\Chrome\\Application\\")
+os.system("chrome.exe//https://drive.google.com/drive/my-drive")
+
+# enter google drive path into browser's search bar
+# baseurl = 'https://www.google.com/search?q='
+# questions = input()
+# url = baseurl + (questions)
